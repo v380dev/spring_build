4d060c0c2188430c2873fa95d733f6888cce2f24
v380dev <v380dev@gmail.com>
2022-08-23 19:47:21 +0300

    init project: Corona desinfector

---
+++ src/main/java/org/example/CoronaDesinfector.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class CoronaDesinfector {
+    public void start (Room room) {
+//        todo повідомити всім присутнім про початок дезінфекції, і попросити всих вийти
+//        todo вигнати всіх хто не вийшов
+        desinfect (room);
+//        todo повідомити присутнім в комнаті що вони можуть повернутися
+
+
+
+    }
+
+    private void desinfect(Room room) {
+        System.out.println("зачитується молитва: 'корона зникни!' - молитва прочитана, вірус відправлено в ад");
+    }
+}
